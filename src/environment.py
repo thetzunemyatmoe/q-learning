0fbbfdf9e82e8a4f51ecade43ab721c4d6dec828
thetzunemyatmoe <thetzunemyatmoe10@gmail.com>
2024-12-02 23:24:34 +0000

    Q learning algorithm complete (For fixed hyperparameters)

--- src/environment.py
+++ src/environment.py
@@ -6,75 +6,66 @@ import matplotlib.pyplot as plt
 class GridEnvironment:
     def __init__(self, size):
         self.size = size
+        # Initialize the original grid configuration
+        self.original_grid = np.zeros([size, size], dtype=int)
 
         # 2D array of all zeros
         self.the_grid = np.zeros([size, size], dtype=int)
 
-        # self.start. self.goal ---> (x, y)
-        self.start, self.goal = self._initialize_positions()
+        # State coordinates (x, y)
+        self.start = (1, 0)
+        self.original_grid[self.start] = 1
+        # Goal coordinates (x, y)
+        self.goal = self._initialize_goal()
+        self.original_grid[self.goal] = 2
 
         # Function to add obstacles in the grid
-        self._add_obstacles()
+        self.obstacle_indexes_array = self._add_obstacles()
 
         # Rover's position = Start position ---> (x, y)
         self.rover = self.start
+        # The grid to train on
+        self.the_grid = self.original_grid.copy()
 
         # Is goal reached?
         self.isReached = False
-
-        self.initialized = False  # Flag to track if the grid has been initialized
-
         # Initialize the plot for dynamic updates
         plt.ion()  # Turn on interactive mode
         self.fig, self.ax = plt.subplots()  # Create a figure and axis for visualization
 
-    # Setting  random start and goal position as (x, y)
-    def _initialize_positions(self):
-        # Starting position / (x, y)
-        start = (random.randint(0, 3), random.randint(0, 3))
+    # Setting  random goal position as (x, y)
 
-        # Goal position / (x, y)
+    def _initialize_goal(self):
+        # Goal position
         goal = (random.randint(self.size - 4, self.size - 1),
                 random.randint(self.size - 4, self.size - 1))
-
-        # No clash between start and goal postion
-        while start == goal:
-            goal = (random.randint(self.size - 4, self.size - 1),
-                    random.randint(self.size - 4, self.size - 1))
-
-        # Mark the start and goal on the grid
-        # self.the_grid[(x,y)] = value
-        self.the_grid[start] = 1
-        self.the_grid[goal] = 2
-
         # (x, y)
-        return start, goal
+        return goal
 
     def _add_obstacles(self):
+        arr = []
 
         # Number of obstacles (25% of the grid)
         num_obstacles = int(self.size * self.size * 0.25)
+        # print(f'Number of obstacles on the grid --> {num_obstacles}\n')
 
         # List of all possible positions on the grid (x,y)
         available_positions = [(i, j) for i in range(self.size) for j in range(
             self.size) if (i, j) != self.start and (i, j) != self.goal]
+        # print(f'Available positions on the grid \n {available_positions}\n')
 
         # Randomly select positions for set number of obstacles --> (x, y)
         obstacle_positions = random.sample(available_positions, num_obstacles)
+        # print(f'Obstacle positions on the grid \n{obstacle_positions}\n')
 
-        # Set obstacles value to -1 on the grid
+        # Mark obstacles as -1 on the grid
         for pos in obstacle_positions:
-            self.the_grid[pos] = -1
+            arr.append((pos[0] * self.size) + pos[1])
+            self.original_grid[pos] = -1
+        arr.sort()
 
-    # Rover action
-    # 1. Calculate the new coordinates based on the action
-    # 2. Get the reward for moving to the new coordinate from reward function
-    # 3. If the new coordinate is valid (Move into an empty cell, Move into the goal cell, Move to visited cell)
-        # a. Update the actual coordinate (x, y) of rover
-        # b. Leave the trace in the grid
-        # c. Mark the new coordiate in the grid with appropriate indicator
-    # Return the calculated reward
-    # direction from ['up', 'down', 'left', 'right]
+        return arr
+
     def move_rover(self, direction):
         # print(self.the_grid)
 
@@ -107,68 +98,11 @@ class GridEnvironment:
             self.the_grid[self.rover] = 3  # Leave a trace
             self.rover = new_position  # Update rover's new position
             self.the_grid[self.rover] = 1  # Mark rover position in the grid
-
-        self.visualize_grid()
+        # self.visualize_grid()
         return reward
 
-    # def move_rover(self, direction):
-    #     # Current rover position (x, y)
-    #     x, y = self.rover
-    #     print('Current x: ', x)
-    #     print('Current y: ', y)
-    #     print('Action take: ', direction)
-
-    #     # Map direction to coordinates change
-    #     if direction == "up":
-    #         possible_new_position = (x - 1, y)
-    #     elif direction == "down":
-    #         possible_new_position = (x + 1, y)
-    #     elif direction == "left":
-    #         possible_new_position = (x, y - 1)
-    #     elif direction == "right":
-    #         possible_new_position = (x, y + 1)
-
-    #     reward = self.get_reward(new_position=possible_new_position)
-
-    #     # Reward -> 50 -> Goal is reached
-    #     if reward == 50:
-    #         self.the_grid[self.rover] = 3  # Leave a trace
-    #         self.rover = possible_new_position
-    #         self.the_grid[self.rover] = 69  # Mark that the goal is reached
-    #     # Reward -> -1 -> Possible path
-    #     elif reward == -1:
-    #         self.the_grid[self.rover] = 3  # Leave a trace
-    #         self.rover = possible_new_position
-    #         self.the_grid[self.rover] = 1  # Mark that the goal is reached
-        # return reward
-
-        # # New_position - (x, y)
-        # # Check if the new position is within bounds and not an obstacle - Move the robot to new position and Leave a trace
-        # if (0 <= possible_new_position[0] < self.size) and (0 <= possible_new_position[1] < self.size) and self.the_grid[possible_new_position] != -1:
-        #     # Mark the old rover position with 3 (trace), if it's not the goal
-        #     if self.the_grid[possible_new_position] != 2:  # Avoid overwriting the goal
-        #         # Mark previous position with trace
-        #         self.the_grid[self.rover] = 3
-
-        #     if self.the_grid[new_position] == 2:
-        #         self.the_grid[new_position] = 69
-        #         print("the agent has reached the goal ")
-        #     # Move the rover to the new position (mark it with 1 for rover)
-        #     self.rover = new_position
-        #     self.the_grid[self.rover] = 1  # Set new rover position to 1 (blue)
-        # else:
-        #     print(
-        #         f"Invalid move to {new_position}. Either out of bounds or an obstacle.")
-        #     return -100, self.get_state_index()
-
-        # Visualize the grid after the move
-        # self.visualize_grid()
-
-        # return self.get_reward(), self.get_state_index()
-
-    # Reward function
-
     def get_reward(self, new_position):
+
         # Penalize for moving out of bound
         if (new_position[0] < 0 or new_position[0] >= self.size) or (new_position[1] < 0 or new_position[1] >= self.size):
             return -20
@@ -182,9 +116,25 @@ class GridEnvironment:
         # Penlaize for revisiting previously visited cells
         elif self.the_grid[new_position] == 3:
             return -2
-        # Penalize for hiiting the obstalce
+            # Penalize for hiiting the obstalce
         elif self.the_grid[new_position] == -1:
-            return -50
+            # print(f'new_position is the obstacle')
+            return -100000
+
+        return 0
+
+    def goal_state_index(self):
+        return (self.goal[0] * self.size) + self.goal[1]
+
+    def get_current_state_index(self):
+        # Convert the 2D position of the rover into a 1D state index
+        return (self.rover[0] * self.size) + self.rover[1]
+
+    def reset(self):
+        # Rover's position = Start position ---> (x, y)
+        self.rover = self.start
+        self.the_grid = self.original_grid.copy()
+        self.isReached = False
 
     def visualize_grid(self):
         # Map the grid values to colors
@@ -213,42 +163,3 @@ class GridEnvironment:
         self.ax.axis('off')  # Hide axis
         plt.draw()  # Redraw the plot
         plt.pause(0.00001)  # Pause to allow visualization to update
-
-    def get_state_index(self):
-        # Convert the 2D position of the rover into a 1D state index
-        return (self.rover[0] * self.size) + self.rover[1]
-
-    def is_terminal_state(self):
-        return self.rover == self.goal or self.the_grid[self.rover] == -1
-
-    def reset(self):
-        # Initialize grid, start, and goal only once
-        if not self.initialized:
-            self.the_grid = np.zeros([self.size, self.size], dtype=int)
-            self.start, self.goal = self._initialize_positions()
-            self.rover = self.start
-            self.the_grid[self.start] = 1
-            self.the_grid[self.goal] = 2
-            self._add_obstacles()
-            self.initialized = True
-        else:
-            # Reset rover to start position and clear any traces
-            self.the_grid = np.where(self.the_grid == 3, 0, self.the_grid)
-            self.the_grid[self.rover] = 0  # Clear previous rover position
-            self.rover = self.start
-            self.the_grid[self.rover] = 1
-            self.the_grid[self.goal] = 2
-
-        self.isReached = False  # Reset goal status
-
-    def get_grid(self):
-        return self.the_grid
-
-    def get_start(self):
-        return self.start
-
-    def get_goal(self):
-        return self.goal
-
-    def get_rover_position(self):
-        return self.rover
--- src/qlearning.py
+++ src/qlearning.py
@@ -2,37 +2,34 @@ import numpy as np
 import random
 from environment import GridEnvironment
 import time
+import os
 
 
 class QLearning:
-    def __init__(self, learning_rate=0.1, discount_factor=0.9, epsilon=0.1):
+    def __init__(self, learning_rate, discount_factor, epsilon, epsilon_decay=0.99, min_epsilon=0.1):
         self.env = GridEnvironment(20)
         # Q-Table for all states and actions
         self.q_table = np.zeros((self.env.size * self.env.size, 4))
+
+        # Hyperparameters
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
         self.epsilon = epsilon
-        self.actions = ['up', 'down', 'left', 'right']  # Possible actions
+        self.epsilon_decay = epsilon_decay
+        self.min_epsilon = min_epsilon
 
-        print(self.q_table.shape)
+        # Possible action in each state
+        self.actions = ['up', 'down', 'left', 'right']
 
     def choose_action(self, current_state):
-        # return random.choice(self.actions)
         # Explore with probability epsilon
         if np.random.rand() < self.epsilon:
             return np.random.choice(self.actions)  # Random action
-
-        # Exploit with probability 1 - epsilon (choose action with highest Q-value)
-        # Index of the highest value
-        actions = np.argmax(self.q_table[current_state, :])
-
-        return self.actions[np.random.choice([actions])]
+        # Exploit with probability 1 - epsilon (choose action with highest Q-value
+        return self.actions[np.argmax(self.q_table[current_state, :])]
 
     def bellham(self, current_state, new_state, action, reward):
-        # print(f'Current state {current_state}')
-        # print(f'Action {action}')
-        # print(f'New State {new_state}')
-        # print(f'Reward {reward}\n------------------')
+
         # Get the Q-value for the current state-action pair
         q_value = self.q_table[current_state, action]
 
@@ -45,29 +42,59 @@ class QLearning:
         self.q_table[current_state, action] = new_q_value
 
     def learn(self, episodes):
-
+        print("Learning has started")
+        steps_per_episode = []
         for episode in range(episodes):
-            print(self.q_table)
+            steps = 0
+            total_reward = 0
             # Resetting the environment
-            print(f'Environemnt setting up for episode {episode}')
-            time.sleep(3)
             self.env.reset()
 
-            while not q_learning.env.isReached:
-                # Current state of the rover (Scalar value acted as an index to access the Q table assiociated with the current state)
-                current_state = self.env.get_state_index()
-
-                # Choose the action
+            while not self.env.isReached:
+                current_state = self.env.get_current_state_index()
                 action = self.choose_action(current_state=current_state)
-                # Retrieve the reward, update the grid according and visualize
-                reward = q_learning.env.move_rover(action)
-
-                # New state of the rover after taking the action (Scalar value acted as an index to access the Q table assiociated with the current state)
-                new_state = self.env.get_state_index()
+                reward = self.env.move_rover(action)
+                new_state = self.env.get_current_state_index()
+                # print(
+                #     f'Current state [{current_state}] + Action [{action}] ---> New State[{new_state}]')
 
                 self.bellham(current_state, new_state,
                              self.actions.index(action), reward)
+                total_reward += reward
+                steps += 1
+
+                # Visualize the grid at each step
+            steps_per_episode.append(steps)
+            print(
+                f'Episode {episode+1}: Total Reward: {total_reward}, Steps: {steps}')
+            # Epsilon decay for exploration vs exploitation
+            self.epsilon = max(
+                self.min_epsilon, self.epsilon * self.epsilon_decay)
+
+         # Optionally, visualize the results or save them
+        print('Training completed. Total steps per episode: ', steps_per_episode)
+        print('---------------------------------------------------')
+
+    def save_data(self):
+        q_table = self.q_table
+        obstacles = self.env.obstacle_indexes_array    # Example obstacle indices
+        goal_state = self.env.goal_state_index()
+
+        np.savez('q_learning_data.npz', q_table=q_table,
+                 obstacles=obstacles, goal_position=goal_state)
+        # Specify directory and filename
+        directory = '/Users/thetzunemyatmoe/Documents'
+        filename = 'q_learning_data.npz'
+        # Ensure the directory exists
+        os.makedirs(directory, exist_ok=True)
+
+        # Save file
+        file_path = os.path.join(directory, filename)
+        np.savez(file_path, q_table=q_table, obstacles=obstacles,
+                 goal_state=goal_state)
+        print(f"Data saved successfully at {file_path}")
 
 
-q_learning = QLearning()
-q_learning.learn(100)
+# q_learning = QLearning(learning_rate=0.3, discount_factor=0.95, epsilon=0.8)
+# q_learning.learn(1500)
+# q_learning.save_data()
